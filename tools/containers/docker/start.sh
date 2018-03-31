b7b27b2ac6d5176ed6d1eea0f2cfff80f5f2b983
Tomek Kalinowski <t.kalinowski@pilot.co>
2018-03-31 16:47:38 +0200

    prevent input variable splitting on some ttys

--- tools/containers/docker/start.sh
+++ tools/containers/docker/start.sh
@@ -7,14 +7,14 @@ else
   docker run -v $(pwd):/pro pro:init
 fi
 
-if [ $1 == 'server' -o $1 == 's' ]; then
+if [ "$1" == 'server' -o "$1" == 's' ]; then
   docker-compose up
-elif [ $1 == 'build' ]; then
+elif [ "$1" == 'build' ]; then
   docker-compose down && docker-compose build
-elif [ $1 == 'console' -o $1 == 'c' ]; then
+elif [ "$1" == 'console' -o "$1" == 'c' ]; then
   docker exec -it `docker-compose ps -q consumer` bash -c "ruby framework/tools/starter/start.rb c"
 
-  if [ $? -ne 0 ]; then
+  if [ "$?" -ne 0 ]; then
     echo 'Is the server running?'
   fi
 fi
